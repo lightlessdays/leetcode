96e7c0b6fb87d7d36063ef841dfefaf7f3770eea
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-07-19 14:25:46 +0530

    Create 00278.java

---
+++ 00278.java
@@ -0,0 +1,22 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start=1;
+        int end=n;
+        int returnValue=start;
+        while(start<=end){
+            int middle=start-(start-end)/2;
+            boolean bad=isBadVersion(middle);
+            if(bad){
+                returnValue=middle;
+                end=middle-1;
+            }
+            else{
+                start=middle+1;
+            }
+        }
+        return returnValue;
+    }
+}
