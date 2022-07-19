167f5f041c6dd7097d110a633d0289cfad9d7986
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-07-19 14:02:48 +0530

    Update 00217.java

--- 00217.java
+++ 00217.java
@@ -6,7 +6,6 @@ LINK: https://leetcode.com/problems/contains-duplicate/
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if (nums == null || nums.length == 0) return false;
         Arrays.sort(nums);
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1]){
